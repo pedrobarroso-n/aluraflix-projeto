04983a10ee0ea4c8a4757e6d7d56d30c62c60c06
pedrobarroso-n <pbn62413@gmail.com>
2024-07-27 11:37:06 -0300

    continuação do Banner

--- src/components/TituloSecao/index.js
+++ src/components/TituloSecao/index.js
@@ -1,7 +1,11 @@
-import styles from './TituloSecao.module.css';
+import './TituloSecao.module.css';
 
 function TituloSecao({ color, children }) {
-    return <h2 style={{ backgroundColor: color }}>{children}</h2>
+    return (
+        <h2 style={{ backgroundColor: color }}>
+            {children}
+        </h2>
+    )
 }
 
 export default TituloSecao;

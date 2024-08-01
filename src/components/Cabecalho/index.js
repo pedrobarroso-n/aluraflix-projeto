bc4852192699741a7cf5b726e9f5b87dd8564e72
pedrobarroso-n <pbn62413@gmail.com>
2024-08-01 19:44:52 -0300

    adicionando normalise css

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -2,37 +2,33 @@ import { Link } from 'react-router-dom';
 import styles from './Cabecalho.module.css';
 
 function Cabecalho() {
-
-    const clickLink = ()=> {
-        const itens = document.querySelectorAll('li');
-        itens.forEach(item => {
-            item.className === '' ? item.className = styles.ativo : item.className = '';
-        })
-    } 
-
     return (
         <header className={styles.container}>
             <img src='/images/logo.png' alt='Logo do projeto react AluraFlix' />
             <nav>
                 <ul className={styles.links}>
-                    <li className={styles.ativo} onClick={clickLink}>  
+                    <li>  
                         <Link to='./'>
-                            <img className={styles.icons}
-                                src="/images/home.png"
+                            <img 
+                                className={styles.icons}
+                                src="/images/icons/home.png"
                                 alt='icone da aba Home do AluraFlix'
                             />
-                            <p>Home</p>
-                        </Link>
-                    </li>
-                    <li onClick={clickLink}>
+                            <p>
+                                Home
+                            </p>
+                        </Link></li>
+                    <li>
                         <Link to='./novovideo'>
-                            <img className={styles.icons}
-                                src="/images/novovideo.png"
+                            <img 
+                                className={styles.icons}
+                                src="/images/icons/novovideo.png"
                                 alt='icone da aba Novo Vídeo do AluraFlix'
                             />
-                             <p>Novo Vídeo</p>
-                        </Link>
-                    </li>
+                            <p>
+                                Novo Vídeo
+                            </p>
+                        </Link></li>
                 </ul>
             </nav>
         </header> 
